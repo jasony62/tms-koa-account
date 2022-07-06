9c6c039413ec30dc7695be796ecfe64106dabd94
jasony62 <jasony62@qq.com>
2022-07-06 23:52:16 +0800

    优化代码；发布0.0.10版本

--- config.js
+++ config.js
@@ -1,42 +1,8 @@
-const fs = require('fs')
-const path = require('path')
-const _ = require('lodash')
-const log4js = require('@log4js-node/log4js-api')
-const logger = log4js.getLogger('tms-koa-account-config')
+const { loadConfig } = require('tms-koa')
 
-/**
- * 获得配置数据
- *
- * @param {string} name - 配置名称
- * @param {object} defaultConfig - 默认配置
- *
- * @return {object} 配置数据对象
- */
- function loadConfig(name, defaultConfig) {
-  let basepath = path.resolve('config', `${name}.js`)
-  let baseConfig
-  if (fs.existsSync(basepath)) {
-    baseConfig = require(basepath)
-    logger.info(`从[${basepath}]加载配置`)
-  } else {
-    logger.warn(`[${name}]配置文件[${basepath}]不存在`)
-  }
-  let localpath = path.resolve('config', `${name}.local.js`)
-  let localConfig
-  if (fs.existsSync(localpath)) {
-    localConfig = require(localpath)
-    logger.info(`从[${localpath}]加载本地配置`)
-  }
-  if (defaultConfig || baseConfig || localConfig) {
-    return _.merge({}, defaultConfig, baseConfig, localConfig)
-  }
-
-  return false
-}
-
-const AccountConfig = (function() {
+const AccountConfig = (function () {
   let accountConfig
-  return function() {
+  return function () {
     if (accountConfig) {
       return accountConfig
     } else {
@@ -45,9 +11,10 @@ const AccountConfig = (function() {
     }
   }
 })()
-const AppConfig = (function() {
+
+const AppConfig = (function () {
   let appConfig
-  return function() {
+  return function () {
     if (appConfig) {
       return appConfig
     } else {
@@ -57,4 +24,8 @@ const AppConfig = (function() {
   }
 })()
 
-module.exports = { AccountConfig: AccountConfig(), AppConfig: AppConfig(), loadConfig }
+module.exports = {
+  AccountConfig: AccountConfig(),
+  AppConfig: AppConfig(),
+  loadConfig,
+}
